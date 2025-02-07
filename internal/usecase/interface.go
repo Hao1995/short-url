b66c3f50ed57ff841d6e4b04b2bcdde9ced94e15
Harry Lu <harry.nan.825@gmail.com>
2025-02-07 14:55:58 +0800

    feat: usecase, define interface

---
+++ internal/usecase/interface.go
@@ -0,0 +1,17 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/Hao1995/short-url/internal/domain"
+)
+
+type Repository interface {
+	Create(ctx context.Context, createDto domain.CreateDto) (string, error)
+	Get(ctx context.Context, url string) (string, error)
+}
+
+type UseCase interface {
+	Create(ctx context.Context, createDto domain.CreateDto) (string, error)
+	Get(ctx context.Context, url string) (string, error)
+}
